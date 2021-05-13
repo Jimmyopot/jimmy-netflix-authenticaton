95e73875f41b85a212e9b443d1e7cb38dc85a963
Jimmyopot <jimmyopot@gmail.com>
2021-05-13 16:57:40 +0300

    Completed project, deployed on firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,14 @@
 import firebase from 'firebase';
+import dotenv from 'dotenv';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBHGM0EAhjW4-Vr-Xhk15QKQkMd2JV-JXs",
+    apiKey: process.env.REACT_APP_API_KEY,
     authDomain: "jimmy-opot-netflix-auth.firebaseapp.com",
     projectId: "jimmy-opot-netflix-auth",
     storageBucket: "jimmy-opot-netflix-auth.appspot.com",
-    messagingSenderId: "211246632021",
-    appId: "1:211246632021:web:7b6078afc6e08c610d58dc",
-    measurementId: "G-L01K2HHHBT"
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
   };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
